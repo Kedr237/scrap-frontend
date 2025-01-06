801ea5383b3ae901abba346a49eb25e29d3f6533
Kedr237 <ll.vanya237@gmail.com>
2025-01-06 17:35:19 +0300

    Receiving notes

--- src/pages/Notes/NotesList/NotesList.js
+++ src/pages/Notes/NotesList/NotesList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getBaseNotes } from "../../../actions/notesActions";
 import "./NotesList.css";
 
@@ -6,19 +6,22 @@ function NotesList() {
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    async function handleGetNotes() {
-        setLoading(true);
-        const notesData = await getBaseNotes();
-        setNotes(notesData);
-        setLoading(false);  
-    };
+
+
+    useEffect(() => {
+        async function fetchNotes() {
+            setLoading(true);
+            const notesData = await getBaseNotes();
+            setNotes(notesData);
+            setLoading(false); 
+        };
+
+        fetchNotes();
+    }, []);
 
     return (
         <div className="NotesList">
-            <button onClick={() => handleGetNotes()}>Get notes</button>
-            <br />
-            <br />
-            {!loading &&
+            {!loading && notes &&
                 <ul>
                     {notes.map((note, index) => (
                         <li key={index}>{note.title}</li>
